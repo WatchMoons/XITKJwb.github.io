4dad3596beb54c39f0843dd633e9b261a41f3335
WatchMoons <89377310+WatchMoons@users.noreply.github.com>
2021-09-23 19:12:40 +0700

    v.0.1
    wkwk

---
+++ js/close.js
@@ -0,0 +1,8 @@
+function scroll() {
+  var x = document.getElementById("scroll");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
